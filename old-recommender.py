1c769fd316f5dcb9d1b5cfa97d55a679425de9eb
Lukasz <lukasz.sromek@gmail.com>
2021-02-05 07:33:38 +0100

    new recommender bot

--- old-recommender.py
+++ old-recommender.py
@@ -115,11 +115,10 @@ except:
     exit(1)
 
 gran = 3600
-fee = 0.00075
-funding_fee = 0.0008
+fee = 0.0075
+funding_fee = 0.0004*10
 
-# params = [46.388868,  19.00115881, 6.98667037, 1.43094999]
-params = [46, 19, 7, 0]
+params = [46.388868,  19.00115881, 6.98667037, 1.43094999]
 window_width = max(params) * 3
 
 it = 0
@@ -139,15 +138,12 @@ while True:
     last_tick = df.iloc[-1]
 
     # we do not want to compound, but if we lose money, we cannot trade with the capital we do not have!
-    # risk = risk_fraction * min(capital_usdt, starting_capital)
-    rick = risk_fraction * capital_usdt
-    # buying_power = leverage * min(capital_usdt, starting_capital)
-    buying_power = leverage * capital_usdt
-
+    risk = risk_fraction * min(capital_usdt, starting_capital)
+    buying_power = leverage * min(capital_usdt, starting_capital)
 
     msg = None
 
-      if not in_position:
+    if not in_position:
 
         if capital_usdt <= 0:
             break
@@ -168,73 +164,43 @@ while True:
             stop_size /= 2
             current_stop += stop_size
 
-            if stop_size <= 10 or stop_size >= 700:  # we need decent stop size to account for fees
+            if stop_size <= 0:
                 continue
 
             in_position = True
             long_position = True
 
-            # position_size_nofee = risk / stop_size
-            # position_size = risk/(stop_size + buy_price *
-            #                       funding_fee + 2*buy_price*fee)
-
-            # print("KACZKA", position_size, position_size_nofee)
-
-            R = 700/stop_size
-
             position_size = risk / stop_size
             position_size = min(buying_power / buy_price, position_size)
 
-            target = buy_price + R * stop_size
-
-            msg = f'Open LONG {position_size:.6f} BTC ({(position_size*buy_price):.2f} USD) at price ${buy_price:.3f}. Stop loss at ${current_stop:.3f} ({current_stop/buy_price*100.0:.2f}%, target at ${target:.2f}, size={stop_size:.3f})'
+            msg = f'Open LONG {position_size:.6f} BTC ({(position_size*buy_price):.2f} USD) at price ${buy_price:.3f}. Stop loss at ${current_stop:.3f} ({current_stop/buy_price*100.0:.2f}%, size={stop_size:.3f})'
 
         elif last_tick['close'] < last_tick['short_entry']:
-
             current_stop = last_tick['short_exit']
             initial_stop = current_stop
 
             sell_price = last_tick['close']
             stop_size = current_stop - sell_price
 
-            # stop_size /= 2
-            # current_stop -= stop_size
-
-            if stop_size <= 10 or stop_size >= 700:
+            if stop_size <= 0:
                 continue
 
             in_position = True
             long_position = False
 
-            # position_size_nofee = risk / stop_size
-            # position_size = risk/(stop_size + sell_price *
-            #                       funding_fee + 2*sell_price*fee)
-
-            # print("KACZKA", position_size, position_size_nofee)
-
-            # if position_size < 0.0001:
-            #     continue
-
             position_size = risk / stop_size
             position_size = min(buying_power / sell_price, position_size)
 
-            R = 700/stop_size
-
-            target = sell_price - R * stop_size
-
-            msg = f'Open SHORT {position_size:.6f} BTC ({(position_size*sell_price):.2f} USD) at price ${sell_price:.3f}. Stop loss at ${current_stop:.3f} ({current_stop/sell_price*100.0:.2f}%, target at ${target:.2f}, size={stop_size:.3f})'
+            msg = f'Open SHORT {position_size:.6f} BTC ({(position_size*sell_price):.2f} USD) at price ${sell_price:.3f}. Stop loss at ${current_stop:.3f} ({current_stop/sell_price*100.0:.2f}%, size={stop_size:.3f})'
 
     else:
-
         if long_position:
             last_stop = current_stop
-            # change stop only if we are not stopped out by current stop!
-            # AND the next stop does not stop us out immediately LOL
-            # if last_tick['low'] > current_stop and last_tick['low'] > last_tick['long_exit']:
-            #     current_stop = max(current_stop, last_tick['long_exit'])
+            if last_tick['low'] > current_stop: # change stop only if we are not stopped out by current stop!
+                current_stop = max(current_stop, last_tick['long_exit'])
 
-            # if current_stop > last_stop:
-            #     msg = f' -- LONG RAISE STOP to ${current_stop:.3f} ({current_stop/buy_price*100.0:.2f}%, size={stop_size:.3f})'
+            if current_stop > last_stop:
+                msg = f' -- LONG RAISE STOP to ${current_stop:.3f} ({current_stop/buy_price*100.0:.2f}%, size={stop_size:.3f})'
 
             if last_tick['low'] <= current_stop:
                 in_position = False
@@ -242,31 +208,20 @@ while True:
                 sell_price = current_stop
                 result = position_size * sell_price * \
                     (1.0 - fee) - position_size * buy_price * \
-                    (1.0 + fee) - position_size * sell_price * funding_fee
+                    (1.0 + fee) - position_size * funding_fee
 
                 capital_usdt += result
-                msg = f' * [stp] CLOSED long position (${buy_price:.3f}) at ${sell_price:.3f} for PnL={result:.3f}. Capital = ${capital_usdt:.3f}'
-            elif last_tick['high'] >= target:
-                in_position = False
-
-                sell_price = target
-                result = position_size * sell_price * \
-                    (1.0 - fee) - position_size * buy_price * \
-                    (1.0 + fee) - position_size * sell_price * funding_fee
-
-                capital_usdt += result
-                msg = f' * [tgt] CLOSED long position (${buy_price:.3f}) at ${sell_price:.3f} for PnL={result:.3f}. Capital = ${capital_usdt:.3f}'
+                msg = f' * CLOSED long position (${buy_price:.3f}) at ${sell_price:.3f} for PnL={result:.3f}. Capital = ${capital_usdt:.3f}'
             else:
                 print(
                     f"Long position {position_size:.6f} BTC @ {buy_price:.3f}. Last close: {last_tick['close']:.3f}. Current stop: {current_stop:.3f}", end='\r')
         else:
             last_stop = current_stop
-            # change stop only if we are not stopped out by current stop!
-            # if last_tick['high'] < current_stop and last_tick['high'] < last_tick['short_exit']:
-            #     current_stop = min(current_stop, last_tick['short_exit'])
+            if last_tick['high'] < current_stop: # change stop only if we are not stopped out by current stop!
+                current_stop = min(current_stop, last_tick['short_exit'])
 
-            # if current_stop < last_stop:
-            #     msg = f' -- SHORT LOWER STOP to ${current_stop:.3f} ({current_stop/sell_price*100.0:.2f}%, size={stop_size:.3f})'
+            if current_stop < last_stop:
+                msg = f' -- SHORT LOWER STOP to ${current_stop:.3f} ({current_stop/sell_price*100.0:.2f}%, size={stop_size:.3f})'
 
             if last_tick['high'] >= current_stop:
                 in_position = False
@@ -274,28 +229,17 @@ while True:
                 buy_price = current_stop
                 result = position_size * sell_price * \
                     (1.0 - fee) - position_size * buy_price * \
-                    (1.0 + fee) - position_size * sell_price * funding_fee
+                    (1.0 + fee) - position_size * funding_fee
 
                 capital_usdt += result
 
-                msg = f' * [stp] CLOSED short position (${sell_price:.3f}) at ${buy_price:.3f} for PnL={result:.3f}. Capital = ${capital_usdt:.3f}'
-            elif last_tick['low'] <= target:
-                in_position = False
-
-                buy_price = target
-                result = position_size * sell_price * \
-                    (1.0 - fee) - position_size * buy_price * \
-                    (1.0 + fee) - position_size * sell_price * funding_fee
-
-                capital_usdt += result
-
-                msg = f' * [tgt] CLOSED short position (${sell_price:.3f}) at ${buy_price:.3f} for PnL={result:.3f}. Capital = ${capital_usdt:.3f}'
+                msg = f' * CLOSED short position (${sell_price:.3f}) at ${buy_price:.3f} for PnL={result:.3f}. Capital = ${capital_usdt:.3f}'
             else:
                 print(
                     f"Short position {position_size:.6f} BTC @ {sell_price:.3f}. Last close: {last_tick['close']:.3f}. Current stop: {current_stop:.3f}", end='\r')
 
     if msg is not None:
-        print(f"[{last_tick.name}] {msg}")
+        print(f"[{datetime.datetime.now()}] {msg}")
         twilio_client.messages.create(
             body=msg,
             from_=source_phone,
